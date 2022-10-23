f277bf2b19fd0190db334a9b426f3ede82f8df46
tsioryRaman <rtsiory4@gmail.com>
2022-10-23 19:44:06 +0300

    feat: done header

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,19 @@ module.exports = {
       colors: {
         'default': '#333',
         "select-color": '#222',
-        "dark":"#1b1b1b"
+        "dark":"#1b1b1b",
+        "dark-gradient":"rgba(0,0,0,0.9)"
       },
       strokeWidth: {
         '4': '4px',
+      },
+      height: {
+        "450":"450px"
       }
     },
     container: {
       center: true,
-    },
+    }
   },
   plugins: [],
 }
