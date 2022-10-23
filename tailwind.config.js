9791dd3eb4672d097939df89af1ae565a19375ae
tsioryRaman <rtsiory4@gmail.com>
2022-10-23 13:57:38 +0300

    deps: add tailwindcss/webpack-encore to project configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+}
